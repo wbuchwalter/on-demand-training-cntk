444f09b630bf032a84b098ac56417779a1ad4351
William Buchwalter <wbuchwalter@gmail.com>
2016-12-06 18:35:33 -0500

    add custom script extension

--- install-tools.sh
+++ install-tools.sh
@@ -2,3 +2,4 @@
 sudo apt-get install -qq linux-headers-`uname -r`
 sudo chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
 sudo sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
+sudo dpkg -i /tools/nvidia-docker_1.0.0.rc.3-1_amd64.deb
