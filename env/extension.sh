7aac115b5240faa98217a1a1d7158b5c967b7e7c
William Buchwalter <wbuchwalter@gmail.com>
2016-12-07 16:25:19 -0500

    merge all scripts into one

---
+++ env/extension.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Install Driver and nvidia-docker
+sudo apt-get install -qq linux-headers-`uname -r`
+sudo chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
+sudo sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
+sudo dpkg -i /tools/nvidia-docker_1.0.0.rc.3-1_amd64.deb
+
+#Get sources and build a docker image
+git clone https://github.com/wbuchwalter/on-demand-training-vsts
+cd on-demand-training-vsts/src
+docker build -t cntk-mnist .
+
+#Run the training, with a volume to save output
+nvidia-docker run --rm -v=/output:/code/output cntk-mnist
--- env/install-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install -qq linux-headers-`uname -r`
-sudo chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
-sudo sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
-sudo dpkg -i /tools/nvidia-docker_1.0.0.rc.3-1_amd64.deb
