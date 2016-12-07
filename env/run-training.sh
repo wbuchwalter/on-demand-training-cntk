645c5a0a7ca9835868dd37114a0e818e8cd53efd
William Buchwalter <wbuchwalter@gmail.com>
2016-12-07 16:09:45 -0500

    update scripts

--- env/run-training.sh
+++ env/run-training.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #docker pull wbuchwalter/cntk-mnist:$1
 #nvidia-docker run --rm -v=/output:/code/output wbuchwalter/cntk-mnist:$1
+git clone https://github.com/wbuchwalter/on-demand-training-vsts
+cd on-demand-training-vsts/src
+docker build -t cntk-mnist .
 nvidia-docker run --rm -v=/output:/code/output cntk-mnist
