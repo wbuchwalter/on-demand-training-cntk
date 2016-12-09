921c1b831f369e2366a7440a8db63c85fd6e6a57
William Buchwalter <wbuchwalter@gmail.com>
2016-12-09 10:19:13 -0500

    Update extension.sh

--- env/extension.sh
+++ env/extension.sh
@@ -8,12 +8,12 @@ sudo dpkg -i /tools/nvidia-docker_1.0.0.rc.3-1_amd64.deb
 
 #Get sources and build a docker image
 cd /home/agent
-sudo git clone https://github.com/wbuchwalter/on-demand-training-vsts > git.txt
-cd on-demand-training-vsts/src > cd.txt
-sudo docker build -t cntk-mnist . > build.txt
+sudo git clone https://github.com/wbuchwalter/on-demand-training-vsts
+cd on-demand-training-vsts/src
+sudo docker build -t cntk-mnist .
 
 #Run the training, with a volume to save output
-sudo nvidia-docker run --rm -v=/home/agent/output:/code/output cntk-mnist > run.txt
+sudo nvidia-docker run --rm -v=/home/agent/output:/code/output cntk-mnist
 
 #Upload output to azure blob storage
 sudo docker run --rm -v=/home/agent/output:/output wbuchwalter/wbuchwalter:az $1
