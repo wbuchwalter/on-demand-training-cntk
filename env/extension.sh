e4473f810ed652dcdfa3fd46f742a32b5f740270
William Buchwalter <wbuchwalter@gmail.com>
2016-12-10 16:41:52 +0100

    Update extension.sh

--- env/extension.sh
+++ env/extension.sh
@@ -8,13 +8,11 @@ sudo dpkg -i /tools/nvidia-docker_1.0.0.rc.3-1_amd64.deb
 
 #Get sources and build a docker image
 cd /home/agent
-sudo touch 0.txt
 sudo git clone https://github.com/wbuchwalter/on-demand-training-vsts
-sudo touch 0.5.txt
 cd on-demand-training-vsts
-sudo touch 1.txt
+
 sudo docker build -f Dockerfile.train -t cntk-mnist .
-sudo touch 2.txt
+
 #Run the training, with a volume to save output
-sudo touch 3.txt
-sudo nvidia-docker run cntk-mnist > 3.txt
+ps ax | grep dpkg
+sudo nvidia-docker run cntk-mnist
