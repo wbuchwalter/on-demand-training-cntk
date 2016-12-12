ade8dd596e7ac1ef39680128088a42c5d1724dda
William Buchwalter <wbuchwalter@gmail.com>
2016-12-12 09:26:32 +0100

    Update upload-result.py

--- src/upload-result.py
+++ src/upload-result.py
@@ -2,6 +2,7 @@ from azure.storage.blob import BlockBlobService
 from azure.storage.blob import ContentSettings
 import os
 
+buildId = sys.argv[1]
 blob_service = BlockBlobService(account_name='gpuvmtemplatedisks530', account_key='9aSfzJqvoasgkzMod9qNJGDabjtSUbibZjjvsnvHaYMatASwF/y9kH2nbTAOKnLb7bLWjdIJUdwzXhTCvO2L/g==')
 
 ouputPath = '../output'
@@ -10,5 +11,5 @@ files = os.listdir(ouputPath)
 for file in files:
   blob_service.create_blob_from_path(
       'output',
-      file,
+      str(file+buildId),
       os.path.join(ouputPath, file))
